79d004083e100aef298cdf80acbe1121354e124c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-01-21 17:08:35 -0600

    *W2: Learned object parameters

--- experiments/review-experiments/js/script.js
+++ experiments/review-experiments/js/script.js
@@ -7,18 +7,19 @@ Here is a description of this template p5 project.
 
 "use strict"
 
-//const = constant
-//examples
-const PI = 3.14159; //value
-const I_LOVE_TO_LEARN = true; //true or false
-const MY_FAVORITE_PROGRAMMING_LANGUAGE = `JavaScript`; //string
-
-//variables
-let numCircles = 10;
-let circleAlpha = 50;
-let circleSizeIncrease = 50;
-// into constants
-const NUM_CIRCLES = 10;
+//CONSTANTS////////////////////////////////////////////////////////////////////
+// //const = constant
+// //examples
+// const PI = 3.14159; //value
+// const I_LOVE_TO_LEARN = true; //true or false
+// const MY_FAVORITE_PROGRAMMING_LANGUAGE = `JavaScript`; //string
+//
+// //variables
+// let numCircles = 10;
+// let circleAlpha = 50;
+// let circleSizeIncrease = 50;
+// // into constants
+// const NUM_CIRCLES = 10;
 
 // setup()
 //
@@ -26,7 +27,7 @@ const NUM_CIRCLES = 10;
 function setup() {
   createCanvas(500,500);
 
-}
+} //setup end
 
 // draw()
 //
@@ -34,13 +35,39 @@ function setup() {
 function draw() {
   background(0);
 
-  circleAlpha = map(mouseX, 0, width, 10, 100);
-  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
-
-  for (let i = 0; i < NUM_CIRCLES; i++) {
-    push();
-    fill(255, circleAlpha);
-    ellipse(width/2, height/2, i * circleSizeIncrease);
-    pop();
+//OBJECT PARAMETERS////////////////////////////////////////////////////////////////////
+  let config = {
+    x: 250,
+    y: 250,
+    width: 200,
+    height: 200,
+    fillColor: {
+      r: 255,
+      g: 255,
+      b: 0,
+    },
+    mode: CENTER
   }
+  drawFancyRect(config);
+
+
+//CONSTANTS////////////////////////////////////////////////////////////////////
+//   circleAlpha = map(mouseX, 0, width, 10, 100);
+//   circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+//
+//   for (let i = 0; i < NUM_CIRCLES; i++) {
+//     push();
+//     fill(255, circleAlpha);
+//     ellipse(width/2, height/2, i * circleSizeIncrease);
+//     pop();
+//   }
+} // draw end
+
+//OBJECT PARAMETERS////////////////////////////////////////////////////////////////////
+function drawFancyRect({ x, y, width, height, fillColor, mode }) {
+  push();
+  fill(fillColor.r, fillColor.g, fillColor.b);
+  rectMode(mode);
+  rect(x, y, width, height);
+  pop();
 }
