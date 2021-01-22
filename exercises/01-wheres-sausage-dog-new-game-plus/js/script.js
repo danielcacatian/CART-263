17f6f6f4a1f2e6dc4d29c99e03aee92eb51e6822
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-01-21 20:04:49 -0600

    E1: Started exercise 1

---
+++ exercises/01-wheres-sausage-dog-new-game-plus/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 01: Catch the Annoying Dog
+Daniel Cacatian
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
