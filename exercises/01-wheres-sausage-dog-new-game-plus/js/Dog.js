a93522cabd572d9903195843a303e627f579c987
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-01-21 20:08:26 -0600

    E1: Added Dog.js OOP
    This will be the dog the user has to click on to catch

---
+++ exercises/01-wheres-sausage-dog-new-game-plus/js/Dog.js
@@ -0,0 +1,36 @@
+class Dog {
+
+  constructor(x, y, image){
+    this.x = x;
+    this.y = y;
+    this.image = image;
+
+    this.caught = false;
+    this.missed = false;
+    this.asleep = false;
+  }
+
+  display(){
+    push();
+    imageMode(CENTER);
+    image(this.image, 0, 0);
+    pop();
+  }
+
+  mousePressed(){
+    if (mouseX > this.x - this.image.width/2 &&
+        mouseX < this.x + this.image.width/2 &&
+        mouseY > this.y - this.image.height/2 &&
+        mouseY < this.y + this.image.height/2 &&
+        !this.asleep) {
+        this.missed = true;
+  }
+  else if(mouseX > this.x - this.image.width/2 &&
+      mouseX < this.x + this.image.width/2 &&
+      mouseY > this.y - this.image.height/2 &&
+      mouseY < this.y + this.image.height/2 &&
+      this.asleep) {
+      this.caught = true;
+    }
+  }
+}
