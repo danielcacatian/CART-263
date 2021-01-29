5ec13aba41f715d7ac22746c64ae5933a23bfe84
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-01-28 21:09:57 -0600

    W3: Completed responseVoice experiment

--- experiments/voices-experiments/js/script.js
+++ experiments/voices-experiments/js/script.js
@@ -5,16 +5,56 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+"use strict";
+
+//RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
+let phrase = `Hello, world!`;
+let saying = ``//Track what is currently being said
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-}
+}//setup() end
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(255);
 
+//RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
+  push();
+  textSize(32);
+  textAlign(CENTER);
+  text(saying, width/2, height/2);
+  pop();
+
+}//draw() end
+
+//RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
+//test
+function mousePressed(){
+  // responsiveVoice.speak("hello world", "UK English Male", {volume: 1});
+  // responsiveVoice.speak("string", "voice", {parameter});
+  // responsiveVoice.speak(`REEEEEEEEEEEEEE`, "UK English Male", {
+  //   pitch: 2,
+  //   rate: 0.2,
+  //   volume: 1
+  // });
+
+  responsiveVoice.speak(phrase, `UK English Male`, {
+    onstart: showSpeaking,
+    onend: hideSpeaking
+  });
+}
+
+function showSpeaking(){
+  saying = phrase;
+}
+
+function hideSpeaking() {
+  saying = ``;
 }
