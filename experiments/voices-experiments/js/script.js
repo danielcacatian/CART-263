d0bbed1d2e0e232cf687ac7dbf47ea28bb0f6c97
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-01-30 21:00:49 -0600

    W3: Learned about Annyang

--- experiments/voices-experiments/js/script.js
+++ experiments/voices-experiments/js/script.js
@@ -7,6 +7,11 @@ Here is a description of this template p5 project.
 
 "use strict";
 
+//ANNYANG////////////////////////////////////////////////////////////////////
+let on = false;
+//The program's face
+let face = `:-|`;
+
 //RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
 let phrase = `Hello, world!`;
 let saying = ``//Track what is currently being said
@@ -17,44 +22,105 @@ let saying = ``//Track what is currently being said
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+//ANNYANG////////////////////////////////////////////////////////////////////
+  if (annyang) {
+    //Create commands
+    let commands = {
+      //Turn light on/off commands
+      'Turn the light on': function() {
+        on = true;
+      },
+      'Turn the light off': function() {
+        on = false;
+      },
+      //Love/hate commands
+      'I love you': love,
+      'I hate you': hate
+    }
+    //Add the commands and start annyang
+    annyang.addCommands(commands);
+    annyang.start();
+
+  }
+// //test
+//   if (annyang) {
+//     let commands = {
+//       // must use '' for word commands
+//       'hello': function() {
+//         alert(`Howdy!`);
+//       },
+//       'goodbye': function() {
+//         alert(`Ciao!`);
+//       }
+//     };
+//     annyang.addCommands(commands);
+//     annyang.start();
+//   }
+
 }//setup() end
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(255);
+
+//ANNYANG////////////////////////////////////////////////////////////////////
+  //If on is true, make the background white, otherwise make it black
+  if (on) {
+    background(255);
+  }
+  else {
+    background(0);
+  }
+
+  push();
+  translate(width/2, height/2);
+  rotate(PI/2);
+  textSize(400);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(face, 0, 0);
+  pop();
 
 //RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
-  push();
-  textSize(32);
-  textAlign(CENTER);
-  text(saying, width/2, height/2);
-  pop();
+  // push();
+  // textSize(32);
+  // textAlign(CENTER);
+  // text(saying, width/2, height/2);
+  // pop();
 
 }//draw() end
 
+//ANNYANG////////////////////////////////////////////////////////////////////
+function love() {
+  face = `:-)`;
+}
+
+function hate() {
+  face = `:-(`;
+}
+
 //RESPONSIVEVOICE////////////////////////////////////////////////////////////////////
 //test
-function mousePressed(){
-  // responsiveVoice.speak("hello world", "UK English Male", {volume: 1});
-  // responsiveVoice.speak("string", "voice", {parameter});
-  // responsiveVoice.speak(`REEEEEEEEEEEEEE`, "UK English Male", {
-  //   pitch: 2,
-  //   rate: 0.2,
-  //   volume: 1
-  // });
-
-  responsiveVoice.speak(phrase, `UK English Male`, {
-    onstart: showSpeaking,
-    onend: hideSpeaking
-  });
-}
-
-function showSpeaking(){
-  saying = phrase;
-}
-
-function hideSpeaking() {
-  saying = ``;
-}
+// function mousePressed(){
+//   // responsiveVoice.speak("hello world", "UK English Male", {volume: 1});
+//   // responsiveVoice.speak("string", "voice", {parameter});
+//   // responsiveVoice.speak(`REEEEEEEEEEEEEE`, "UK English Male", {
+//   //   pitch: 2,
+//   //   rate: 0.2,
+//   //   volume: 1
+//   // });
+//
+//   responsiveVoice.speak(phrase, `UK English Male`, {
+//     onstart: showSpeaking,
+//     onend: hideSpeaking
+//   });
+// }
+//
+// function showSpeaking(){
+//   saying = phrase;
+// }
+//
+// function hideSpeaking() {
+//   saying = ``;
+// }
