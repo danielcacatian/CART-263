eaa4cee9a363b3a13187b74cb2bf60a9e1309276
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-02-04 13:50:43 -0600

    E2: Atom beautify
    Beautified code

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -8,141 +8,141 @@ Added additional content to the Activity: Slamina by Pippin Barr
 "use strict";
 
 //Animal names
-const animals =  [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
 ];
 
 const BG_COLOR = `#119851`;
@@ -152,7 +152,7 @@ let currentAnswer = ``;
 let score = 0;
 let correctSFX = undefined;
 
-let timeLimit = 60;//1min time limit
+let timeLimit = 60; //1min time limit
 let start = false;
 
 let state = `title`;
@@ -169,6 +169,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  //Annyang commands
   if (annyang) {
     let commands = {
       'I think it is *animal': guessAnimal
@@ -181,7 +182,7 @@ function setup() {
     textAlign(CENTER, CENTER);
   }
 
-}//setup() end
+} //setup() end
 
 
 // draw()
@@ -191,36 +192,33 @@ function draw() {
   background(BG_COLOR);
 
   //States
-  if(state === `title`){
+  if (state === `title`) {
     title()
-  }
-  else if(state === `controls`){
+  } else if (state === `controls`) {
     controls();
-  }
-  else if(state === `simulation`){
+  } else if (state === `simulation`) {
     simulation();
-  }
-  else if(state === `end`){
+  } else if (state === `end`) {
     end();
   }
 
 
-}//draw() end
+} //draw() end
 
 //STATES//////////////////////////////////////////////////////////////////////
 //Title screen state////////////////////////////////////////////////////////////
-function title(){
+function title() {
   //Title
   displayText(`- SLAMINA+ -
-the animal guessing game`, width/2, height/2, 74, BOLD);
+the animal guessing game`, width / 2, height / 2, 74, BOLD);
   //Instructions
-  displayText(`Press 'any key' to continue`, width/2, height-200, 32, NORMAL);
+  displayText(`Press 'any key' to continue`, width / 2, height - 200, 32, NORMAL);
 }
 
 //Controls state////////////////////////////////////////////////////////////
-function controls(){
+function controls() {
   //Text
-  displayText(`- CONTROLS -`, width/2, 300, 74, BOLD);
+  displayText(`- CONTROLS -`, width / 2, 300, 74, BOLD);
   //Controls
   displayText(`GOAL: Guess as many animals as possible within the time limit
 
@@ -230,24 +228,23 @@ function controls(){
 
 - Use 'mouse click' to either skip or move onto the next prompt
 
-- The timer will only go down when you're attempting a guess`
-, width/2, 550, 32, NORMAL);
+- The timer will only go down when you're attempting a guess`, width / 2, 550, 32, NORMAL);
   //Instructions
-  displayText(`Press 'any key' to START`, width/2, height-200, 32, NORMAL);
+  displayText(`Press 'any key' to START`, width / 2, height - 100, 32, NORMAL);
 
 }
 
 //Simulation state////////////////////////////////////////////////////////////
-function simulation(){
+function simulation() {
   //Click to begin
-  if (!start){
-  displayText(`Use 'mouse click' to begin!
-(listen carefully)`, width/2, height/2, 32, NORMAL);
+  if (!start) {
+    displayText(`Use 'mouse click' to begin!
+(listen carefully)`, width / 2, height / 2, 32, NORMAL);
   }
   //Display answer
   //Answer is correct!
-  if (currentAnswer === currentAnimal && start ) {
-    fill(0, 255, 0);//Answer turns green
+  if (currentAnswer === currentAnimal && start) {
+    fill(0, 255, 0); //Answer turns green
     addScore();
     //Play sound effect
     if (!correctSFX.isPlaying()) {
@@ -256,46 +253,46 @@ function simulation(){
   }
   //Answer is wrong!
   else {
-    fill(255, 0, 0);//Answer turns red
+    fill(255, 0, 0); //Answer turns red
   }
-  text(currentAnswer, width/2, height/2);//Displays the guessed answer
+  text(currentAnswer, width / 2, height / 2); //Displays the guessed answer
 
   //Score
-  displayText(`Current score: `+score, 200, 100, 32, NORMAL);
+  displayText(`Current score: ` + score, 200, 100, 32, NORMAL);
 
   //Timer
-  displayText(`Time: `+timeLimit, width-200, 100, 32, NORMAL);
-  if (frameCount % 60 == 0 && timeLimit > 0 && start){
-    timeLimit --;
+  displayText(`Time: ` + timeLimit, width - 200, 100, 32, NORMAL);
+  if (frameCount % 60 == 0 && timeLimit > 0 && start) {
+    timeLimit--;
   }
   //Time's up
-  if (timeLimit === 0){
+  if (timeLimit === 0) {
     state = `end`;
   }
 
 }
 
 //End state////////////////////////////////////////////////////////////
-function end(){
+function end() {
   //Your score
-  displayText(`FINAL SCORE: `+score, width/2, height/2-100, 74, BOLD);
+  displayText(`FINAL SCORE: ` + score, width / 2, height / 2 - 100, 74, BOLD);
   //Restart
-  displayText(`To restart press 'ENTER'`, width/2, height/2+100, 32, NORMAL);
+  displayText(`To restart press 'ENTER'`, width / 2, height / 2 + 100, 32, NORMAL);
 }
 
 
 //ADDITONAL FUNCTIONS////////////////////////////////////////////////////////////
 //Mouse click function
-function mousePressed(){
+function mousePressed() {
   //User skips prompt
-  if(state === `simulation`){
+  if (state === `simulation`) {
     skip();
     start = true;
   }
 }
 
 //Function that skips the prompt
-function skip(){
+function skip() {
   currentAnimal = random(animals);
   let reverseAnimal = reverseString(currentAnimal);
   responsiveVoice.speak(reverseAnimal);
@@ -304,13 +301,13 @@ function skip(){
 }
 
 //Ensures the answer is in lowercase
-function guessAnimal(animal){
+function guessAnimal(animal) {
   currentAnswer = animal.toLowerCase();
   console.log(currentAnswer);
 }
 
 //Function reverses the provided string
-function reverseString(string){
+function reverseString(string) {
   //Split the string into an array of characters
   let characters = string.split('');
   //Reverse the array of characters
@@ -322,13 +319,13 @@ function reverseString(string){
 }
 
 //Score function (adds points by 1)
-function addScore(){
+function addScore() {
   noLoop();
   score++;
 }
 
 //Time goes down
-function timerDecrease(){
+function timerDecrease() {
   timeLimit--;
 }
 
@@ -344,14 +341,12 @@ function displayText(string, x, y, size, style) {
 }
 
 //Key press function
-function keyPressed(){
-  if (state === `title`){
+function keyPressed() {
+  if (state === `title`) {
     state = `controls`;
-  }
-  else if (state === `controls`){
+  } else if (state === `controls`) {
     state = `simulation`;
-  }
-  else if (state === `end` && keyCode === ENTER){
+  } else if (state === `end` && keyCode === ENTER) {
     location.reload();
   }
 }
