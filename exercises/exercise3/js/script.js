467caa5acdd238b572980ae6c1bd64685889fc8c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-02-09 18:54:43 -0600

    E3: Copy/Pasted activity 4 code
    Copy/Pasted activity 4 code and will build on it from here

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -4,29 +4,84 @@
 Spy Profile Generator+
 Daniel Cacatian
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This exercise will mostly operate like the activity assigned
+by Pippin Barr with some modifications and improvements.
 */
+
+let spyProfile = {
+  name: `**REDACTED**`,
+  password: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  task: `**REDACTED**`,
+};
+
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
 
 /**
 Description of preload
 */
 function preload() {
-
-}
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+  objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+  tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
+}//preload() end
 
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  if (data !== null) {
+    let password = prompt(`Agent! What is your password?!`);
+    if (password === data.password) {
+      spyProfile.name = data.name;
+      spyProfile.alias = data.alias;
+      spyProfile.secretWeapon = data.secretWeapon;
+      spyProfile.password = data.password;
+    }
+  }
+  else {
+    generateSpyProfile();
+  }
+}//setup() end
+
+//Fills in the profile prompts with random data
+function generateSpyProfile(){
+  spyProfile.name = prompt(`Agent! What is your name?!`);
+  let instrument = random(instrumentData.instruments);
+  spyProfile.alias = `The ${instrument}`;
+  spyProfile.secretWeapon = random(objectData.objects);
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
+
+  localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(255);
 
-}
+  let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
+
+Name: ${spyProfile.name}
+Alias: ${spyProfile.alias}
+Secret Weapon: ${spyProfile.secretWeapon}
+Password: ${spyProfile.password}`;
+
+  push();
+  textFont(`Courier, monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
+
+}//draw() end
