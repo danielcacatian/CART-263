934844cdbc3b520e14b35a6ef9518063c024784c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-02-09 18:09:59 -0600

    E3: Started exercise 3

---
+++ exercises/exercise3/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+Spy Profile Generator+
+Daniel Cacatian
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
