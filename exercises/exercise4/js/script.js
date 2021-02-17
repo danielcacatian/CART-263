780ea6220ce69bcb058ba55649f3a7fa21987969
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-02-16 23:16:32 -0600

    E4: Added states
    Added a loading screen, title, instructions, simulation, win and lose screen

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -19,7 +19,9 @@ let bubble = undefined;
 //The fish
 let fish = undefined;
 //Score
-let score = 10;
+let score = 11;
+
+let state = `win`;
 
 /**
 Description of setup
@@ -36,6 +38,7 @@ function setup() {
     flipHorizontal: true
   }, function() {
     console.log(`Model loaded.`);
+    // state = `title`;
   });
 
   // Listen for predictions
@@ -79,36 +82,79 @@ Description of draw()
 function draw() {
   background(0, 119, 190);
 
-  if (predictions.length > 0) {
-    let hand = predictions[0];
-    let index = hand.annotations.indexFinger;
-    let tip = index[3];
-    let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    let baseX = base[0];
-    let baseY = base[1];
-
-    displayPin(baseX, baseY, tipX, tipY);
-
-    // Check bubble popping
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size/2) {
-      bubbleReset();
-      score--;
-    }
+  if(state === `loading`){
+    loading();
+  }
+  else if(state === `title`){
+    title();
+  }
+  else if(state === `instructions`){
+    instructions();
+  }
+  else if(state === `simulation`){
+    simulation();
+  }
+  else if(state === `win`){
+    win();
+  }
+  else if(state === `lose`){
+    lose();
   }
 
-  // Score
-  displayText(`${score}`, width/2, height/2, 64, CENTER, CENTER)
+}//draw()
 
-  // Bubble
+//STATE FUNCTIONS/////////////////////////////////////////////////////////
+//Loading state
+function loading(){
+  displayText(`Loading ml5 Handpose...`, width/2, height/2, 32, CENTER, CENTER, BOLD);
+}
+
+//Title state
+function title(){
+  //Bubble
+  bubble.x = width/2;
+  bubble.y = height/2;
   displayBubble();
+  //Title
+  displayText(`Bubble Popper+`, width/2, height/2, 54, CENTER, CENTER, BOLD);
+  displayText(`Press 'SPACE' to continue`, width/2, height/2+100, 18, CENTER, CENTER, NORMAL);
+}
 
-  // Fish
+//Instructions state
+function instructions(){
+  //Controls
+  displayText(`INSTRUCTION:
+
+- Pop 10 bubbles with your
+index finger to win
+
+- DONT pop the fish that
+swims across or you lose!`,
+25, height/2, 24, LEFT, CENTER, BOLD);
+    //Cursor
+    displayPin();
+    displayText(`Pop to START`, width/4*3.25, height/4*2.75, 18, CENTER, CENTER, NORMAL);
+    //Bubble
+    bubble.x = width/4*3.25;
+    bubble.y = height/2;
+    displayBubble();
+}
+
+//Simulation state
+function simulation(){
+  // Score
+  displayText(`${score}`, width/2, height/2, 64, CENTER, CENTER, BOLD)
+  if(score === 0){
+    state = `win`;
+  }
+
+  //Pin
+  displayPin();
+  //Bubble
+  displayBubble();
+  //Fish
   displayFish();
-
-  //Adds movement to objects
+  //Velocity of objects
   velocity()
 
   // Resets bubble/fish if out of bounds
@@ -119,9 +165,71 @@ function draw() {
     fishReset();
   }
 
-}//draw()
+}
+
+//Win state
+function win(){
+  //Text
+  displayText(`YOU WON!`, width/2, height/2, 64, CENTER, CENTER, BOLD);
+  displayText(`Press 'SPACE' to restart if you wish`, width/2, height/2+100, 18, CENTER, CENTER, NORMAL);
+}
+
+//Lose state
+function lose(){
+  //Text
+  displayText(`YOU LOST!`, width/2, height/2, 64, CENTER, CENTER, BOLD);
+  displayText(`Press 'SPACE' to restart if you wish`, width/2, height/2+100, 18, CENTER, CENTER, NORMAL);
+}
 
 //ADDITIONAL FUNCTIONS/////////////////////////////////////////////////////////
+// Function to display pin
+function displayPin(){
+  if (predictions.length > 0) {
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    // Pin body
+    push();
+    noFill();
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+    // Pin head
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(baseX, baseY, 20);
+    pop();
+
+    // Check bubble popping
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size/2) {
+      if(state === `instructions`){
+        state = `simulation`;
+      }
+      else if(state === `simulation`){
+        bubbleReset();
+        score--;
+      }
+    }
+
+    // Check fish getting pocked
+    let d2 = dist(tipX, tipY, fish.x, fish.y);
+    if (d2 < fish.size/2) {
+      state = `lose`;
+    }
+
+  }
+}
+
 // Function to display bubble
 function displayBubble(){
   push();
@@ -129,24 +237,6 @@ function displayBubble(){
   stroke(190, 231, 255);
   strokeWeight(4);
   ellipse(bubble.x, bubble.y, bubble.size);
-  pop();
-}
-
-// Function to display pin
-function displayPin(baseX, baseY, tipX, tipY){
-  // Pin body
-  push();
-  noFill();
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  line(baseX, baseY, tipX, tipY);
-  pop();
-
-  // Pin head
-  push();
-  noStroke();
-  fill(255, 0, 0);
-  ellipse(baseX, baseY, 20);
   pop();
 }
 
@@ -187,12 +277,29 @@ function fishReset(){
 }
 
 //Function to easily add text
-function displayText(string, x, y, size, align1, align2) {
+function displayText(string, x, y, size, align1, align2, style) {
   push();
-  textStyle(BOLD);
+  textStyle(style);
   textAlign(align1, align2);
   textSize(size);
   fill(255);
   text(string, x, y);
   pop();
+}
+
+//Function that contains key presses
+function keyPressed(){
+  //Press SPACE to continue
+  if(keyCode === 32 && state === `title`){
+    state = `instructions`;
+  }
+  else if(keyCode === 32 && state === `win` || state === `lose`){
+    state = `instructions`;
+    score = 11;
+    //Reset everything
+    fish.x = 0-fish.size/2;
+    fish.y = random(height);
+    bubble.x = random(width);
+    bubble.y = height+bubble.size/2;
+  }
 }
