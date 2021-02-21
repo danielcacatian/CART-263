a3399a511b1ef356a7d86b97d156893a344c3047
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-02-20 21:44:44 -0600

    P1: Typewriter.js added
    Typewriter.js is what makes it so a string is typed out in real time on screen

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,32 +1,50 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+The Matrix
+Daniel Cacatian
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This project 1 will be based off the famous scene from The Matrix which
+featured Neo discovering the Matrix. It won't be an accurate representation, but
+it will be heavily inspired from that scene and I will attempt to make the
+individual feel like, the one, Neo.
 */
 
-/**
+let typewriter;
+
+/******************************************************************************
 Description of preload
-*/
+*//////////////////////////////////////////////////////////////////////////////
 function preload() {
 
-}
+}//preload() end
 
 
-/**
+/******************************************************************************
 Description of setup
-*/
+*//////////////////////////////////////////////////////////////////////////////
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-}
+  typewriter = new Typewriter();
+
+}//setup() end
 
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
+  typewriter.display();
 
+}//draw() end
+
+/******************************************************************************
+This section contains additional functions such as key presses,
+display functions, etc.
+*//////////////////////////////////////////////////////////////////////////////
+
+function keyPressed(){
+  typewriter.typewrite(`Wake up, Neo...`, 200, 200);
 }
