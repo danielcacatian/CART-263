9d508d6c9bf22d8ab8b0e9d032c77437c00faca4
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-06 18:48:53 -0600

    W7: Learned an overview of DOM

--- experiments/DOM-experiments/js/script.js
+++ experiments/DOM-experiments/js/script.js
@@ -1,32 +1,52 @@
-"use strict";
+//Use getElementById//////////////////////////////////////////////////////////
+let mainHeading = document.getElementById(`main-heading`);
+//Style
+//with brackets
+mainHeading.style[`color`] = `#339966`;
+mainHeading.style[`font-size`] = `4rem`;
+//or camel-case
+mainHeading.style.fontFamily = `Courier, monospace`;
+mainHeading.style.backgroundColor = `red`;
+//Inner text
 
-/**
-Title of Project
-Author Name
+let pronoun = document.getElementById(`pronoun`);
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-/**
-Description of preload
-*/
-function preload() {
-
+pronoun.innerHTML = `<strong>you</strong>`;
+if (pronoun.innerText === `we`){
+  pronoun.innerText = `you`;
 }
 
+let wellSection = document.getElementById(`well-section`);
 
-/**
-Description of setup
-*/
-function setup() {
+wellSection.style[`color`] = `blue`;
 
+//Change image (setAttribute)//////////////////////////////////////////////////
+let image = document.getElementById(`clown-image`);
+
+image.setAttribute(`src`, `http://loremflickr.com/320/240/clown`);
+
+//Use getElementsByClassName//////////////////////////////////////////////////
+// let headers = document.getElementsByClassName(`header`);
+//or
+let headers = document.querySelectorAll(`.header`);
+
+for (let i = 0; i < headers.length; i++){
+  headers[i].style[`color`] = `#ff0000`;
 }
 
+//Use getElementsByTagName//////////////////////////////////////////////////
+let h2s = document.getElementsByTagName(`h2`);
 
-/**
-Description of draw()
-*/
-function draw() {
-
+for (let i = 0; i < h2s.length; i++){
+  h2s[i].style[`font-size`] = `2rem`;
 }
+
+//Adding onto the HTML webpage//////////////////////////////////////////////////
+let newP = document.createElement(`p`);
+newP.innerText = `Gosh, I sure do like clowns.`;
+
+let clownSection = document.getElementById(`clown-section`);
+clownSection.appendChild(newP);
+
+//Removing elements////////////////////////////////////////////////////////////
+mainHeading.parentElement.removeChild(mainHeading);
