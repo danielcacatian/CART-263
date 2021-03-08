dcb7065cbb400650437b6ded358212069a40ca81
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-08 00:47:09 -0600

    A6: Completed haiku generator activity

--- activities/06-haiku-generator/js/script.js
+++ activities/06-haiku-generator/js/script.js
@@ -1,32 +1,84 @@
+/**
+Haiku Generator
+Pippin Barr
+
+Generates a random haiku!
+*/
+
 "use strict";
 
-/**
-Title of Project
-Author Name
+let fiveSyllableLines = [
+  `O, to be a tree`,
+  `The cat does not know`,
+  `We are all forests`,
+  `You have done your best`,
+  `They are all gone now`
+];
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+let sevenSyllableLines = [
+  `Say the things left unsaid`,
+  `Never believe the wind's lies`,
+  `The autumn stretches its legs`,
+  `Nothing can satisfy you`,
+  `They will not come back again`
+];
 
-/**
-Description of preload
-*/
-function preload() {
 
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+let line1P = document.getElementById(`line-1`);
+let line2P = document.getElementById(`line-2`);
+let line3P = document.getElementById(`line-3`);
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener(`click`, lineClicked);
+line2P.addEventListener(`click`, lineClicked);
+line3P.addEventListener(`click`, lineClicked);
+
+
+function lineClicked(event){
+  fadeOut(event.target, 1);
 }
 
-
-/**
-Description of setup
-*/
-function setup() {
-
+function fadeOut(element, opacity){
+  opacity -= 0.01;
+  element.style[`opacity`] = opacity;
+  if (opacity > 0){
+    requestAnimationFrame(function(){
+      fadeOut(element, opacity);
+    });
+  }
+  else {
+    setNewLine(element);
+    fadeIn(element, 0);
+  }
 }
 
+function fadeIn(element, opacity){
+  opacity += 0.01;
+  element.style[`opacity`] = opacity;
+  if(opacity < 1){
+    requestAnimationFrame(function(){
+      fadeIn(element, opacity);
+    });
+  }
+}
 
-/**
-Description of draw()
-*/
-function draw() {
+function setNewLine(element){
+  if(element === line1P || element === line3P){
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if(element === line2P){
+    element.innerText = random(sevenSyllableLines);
+  }
+}
 
+function random(array){
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
