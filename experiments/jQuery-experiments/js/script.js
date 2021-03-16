f1fbf4b3d59a9e1bb408d3f1bad15aaa059e9204
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-15 21:48:11 -0500

    W8: Learned about jQuery

--- experiments/jQuery-experiments/js/script.js
+++ experiments/jQuery-experiments/js/script.js
@@ -1,32 +1,53 @@
 "use strict";
 
-/**
-Title of Project
-Author Name
+// $ = function name
+// $(`#id-name`);
+let $mainHeading = $(`#main-heading`);
+// css() = style.
+// css(`css attribute`, `value`);
+$mainHeading.css(`color`, `#339966`);
+$mainHeading.css(`font-size`, `4rem`);
+// OR
+$(`#main-heading`).css(`color`, `#ff0000`);
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+// For class
+// If CSS is used like this, it is required to write the css attribute in "quote marks" or cammelCase
+$(`.header`).css({
+  "color": `blue`,
+  "background-color": `black`
+});
 
-/**
-Description of preload
-*/
-function preload() {
+// Changing HTMl text
+// $().text(`string`);
+let spanText = $(`#example-span`).text();
+let reversedSpanText = spanText.split(``).reverse().join(``);
+$(`#example-span`).text(reversedSpanText);
 
+// Changing the HTML tag
+// $().html(`<p> </p>`);
+let spanHTML = $(`#example-span`).html();
+$(`#example-span`).html(`<strong>${spanHTML}</strong>`);
+
+// Attributes
+// attr(`contenteditable`, `true`) --> allows to manually edit the content
+$(`#main-heading`).attr(`contenteditable`, `true`);
+
+// attr(`href`) = `element link` )
+let $link = $(`#thicc-link`);
+if ($link.attr(`href`) === `https://thi.cc`){
+  $link.text(`THICC`);
 }
 
+// Create/Add elements onto HTML
+//$(`<p> ONLY THE TAGS </p>`);
+let $p = $(`<p></p>`);
+$p.text(`Fresh, fresh paragraph!`);
+// How to put it and where
+// --> append(html element) *after the element
+// --> prepend() *before
+$(`#second-section`).append($p);
+// OR
+$(`h2`).after($p); //After each h2
 
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+// Remove something
+// $(element-name).remove();
