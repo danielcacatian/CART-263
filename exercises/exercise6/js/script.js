2508b963a14e74cf787e0d0331dbc28e5521b86c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-17 22:49:02 -0500

    E6: Added a lose screen
    If ALL the secrets are revealed, the player loses.

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
+let secretsRevealed = 0;
+
 $(`.top-secret`).on(`click`,redact);
 setInterval(revelation, 500);
 
 function redact(event){
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
+
+  secretsRevealed--;
 }
 
 function revelation(){
@@ -17,5 +21,17 @@ function attemptReveal(){
   if (r < 0.1) {
     $(this).removeClass(`redacted`);
     $(this).addClass(`revealed`);
+
+    secretsRevealed++;
+  }
+}
+
+//Lose screen pops up
+function draw(){
+  if(secretsRevealed <= 10){
+    $(`#lose-screen`).hide();
+  }
+  else {
+    $(`#lose-screen`).show();
   }
 }
