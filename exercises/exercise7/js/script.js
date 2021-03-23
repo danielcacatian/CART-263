89bcf515132b46ee700c95a2541342291707fa91
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-22 20:41:03 -0500

    E7: Copy/pasted code
    Copy/pasted the original code from the Activity 8 and paste it into the exercise files.

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -4,6 +4,35 @@
 Code Taker++
 Daniel Cacatian
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Da Vinki???
+Add-on to Activity 8: Code Taker created by Pippin Barr.
 */
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event){
+  $(this).addClass(`found`, 500);
+  $(`.secret`).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`, 500);
+    // Check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
