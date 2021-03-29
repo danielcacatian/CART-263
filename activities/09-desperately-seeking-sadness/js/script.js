e69ac943a09fa49be48420ccbf60995f77cf3ae0
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-28 21:26:01 -0500

    A9: Completed Desperately Seeking Sadness activity

--- activities/09-desperately-seeking-sadness/js/script.js
+++ activities/09-desperately-seeking-sadness/js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+/**
+Desperately Seeking Sadness
+Pippin Barr
+
+An emoji n search of satisfying sadness in a world of positivity
+*/
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: `arcade`
+  },
+  scene: [Boot, Play]
+};
+
+let game = new Phaser.Game(config);
