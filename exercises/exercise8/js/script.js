958470e932f2ed8f509fcac9af2cc056b4da433c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2021-03-29 20:59:01 -0500

    E8: Copy/pasted code from activity 9
    Copy pasted all files/code from the original activity as a base for this exercise.

--- exercises/exercise8/js/script.js
+++ exercises/exercise8/js/script.js
@@ -7,3 +7,15 @@ Daniel Cacatian
 An add-on to the already existing activity of Desperately
 Seeking Sadness by Pippin Barr
 */
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: `arcade`
+  },
+  scene: [Boot, Play]
+};
+
+let game = new Phaser.Game(config);
